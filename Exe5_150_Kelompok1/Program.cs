2d4264de058f754a1d92cfca625d5bd55455dcb8
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-12-23 15:51:26 +0700

    Add class Node

--- Exe5_150_Kelompok1/Program.cs
+++ Exe5_150_Kelompok1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Exe5_150_Kelompok1
+{
+    class Node
+    {
+        public string name;
+        public Node next;
+    }
+}
