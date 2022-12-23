925f2678743fa6d72ebae9a610c2f70d2c89fcb3
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2022-12-23 16:31:06 +0700

    Add Method Switch case

--- Exe5_150_Kelompok1/Program.cs
+++ Exe5_150_Kelompok1/Program.cs
@@ -51,7 +51,7 @@ namespace Exe5_150_Kelompok1
                 return;
             }
             Node Current;
-            for (Current = front; Current != null; Current = Current.next);
+            for (Current = front; Current != null; Current = Current.next)
             Console.WriteLine(Current.name);
 
         }
@@ -60,7 +60,52 @@ namespace Exe5_150_Kelompok1
     {
         static void Main(string[] args)
         {
-
+            queue q = new queue();
+            char ch;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("======Menu======");
+                    Console.WriteLine("1. Enter");
+                    Console.WriteLine("2. Delete");
+                    Console.WriteLine("3. Display");
+                    Console.WriteLine("4. Exit");
+                    Console.WriteLine("\nSilahkan Pilih 1 - 4");
+                    ch = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                q.insert();
+                            }
+                            break;
+                        case '2':
+                            {
+                                q.delete();
+                            }
+                            break;
+                        case '3':
+                            {
+                                q.display();
+                            }
+                            break;
+                        case '4':
+                                return;
+                                default:
+                            {
+                                Console.WriteLine("Opsi Tidak Tersedia");
+                            }
+                            break;
+                            
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Silahkan cek");
+                }
+            }
         }
     }
 }
